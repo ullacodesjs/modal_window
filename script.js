44e599e22136d5caeccb28a275fa299b8d1d5a07
Ulla Angell <ullaangell15@gmail.com>
2022-02-14 12:25:21 +0000

    working with css classes

--- script.js
+++ script.js
@@ -4,8 +4,21 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
-console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent);
-}
+const openModal = function () {
+  console.log("button clicked");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", openModal);
+
+// two ways to close the modal with the icon or the background
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
