65591a64f9798edc5b9a5fa61a305fcc7018b9f7
Ulla Angell <ullaangell15@gmail.com>
2022-02-14 14:58:58 +0000

    last commit: esc keypress event

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
 const openModal = function () {
-  console.log("button clicked");
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
@@ -22,3 +21,11 @@ for (let i = 0; i < btnsOpenModal.length; i++)
 // two ways to close the modal with the icon or the background
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  console.log(e.key);
+
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
